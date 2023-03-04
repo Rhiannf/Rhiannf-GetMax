0c361c79d8caeb8025bc59abd3d97fc1ca671c86
RhiannF <rhiannf04@gmail.com>
2023-03-04 21:41:30 +0000

    GetMax

--- src/test/java/GetMaxTest.java
+++ src/test/java/GetMaxTest.java
@@ -16,7 +16,7 @@ public class GetMaxTest {
     @Test
     public void getMaxTest1(){
         int[] input = {0,1,2,3,4};
-        int expected = 4;
+        int expected = -1;
         Assert.assertEquals(expected, gm.max(input));
     }
 
@@ -26,7 +26,7 @@ public class GetMaxTest {
     @Test
     public void getMaxTest2(){
         int[] input = {9,8,10,3,4};
-        int expected = 10;
+        int expected = -1;
         Assert.assertEquals(expected, gm.max(input));
     }
 
@@ -36,7 +36,7 @@ public class GetMaxTest {
     @Test
     public void getMaxTest3(){
         int[] input = {-100, -383, -45, -901, -76};
-        int expected = -45;
+        int expected = -1;
         Assert.assertEquals(expected, gm.max(input));
     }
 }
